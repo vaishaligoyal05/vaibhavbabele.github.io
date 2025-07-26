7d4d15d8a4744668fc68b8920cbbfca60a534233
Nikita Mishra <nikita.mishra.pri@gmail.com>
2025-07-26 17:29:05 +0530

    Enhancement/form update (#116)
    * Add files via upload
    
    * Update README.md
    
    * Add files via upload
    
    * Add files via upload
    
    * Update README.md
    
    * Add files via upload
    
    * Update README.md
    
    * Update README.md
    
    * Add files via upload
    
    PR Introduction
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * Update README.md
    
    * Delete images/Certificate exp.png
    
    * Add files via upload
    
    * Delete images/Certificate exp.png
    
    * Add files via upload
    
    * Update README.md
    
    * Feedback form Update
    
    * Feedback form Update
    
    * Feedback form Update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const element = document.getElementById('scroll-hide');
-const mode=document.getElementById("mode");
+const mode =document.getElementById("mode");
 const load=document.getElementById("onload");
 function checkWindowSize() {
     if (window.innerWidth < 1000) {
@@ -31,17 +31,15 @@ mode.onclick=function(){
     const wasDarkmode = localStorage.getItem('mode') === 'true';
     localStorage.setItem('mode', !wasDarkmode);
     document.body.classList.toggle("dark-mode",!wasDarkmode);
-    if(document.body.classList.contains("dark-mode")){
-        mode.src="../images/sun.png";
-    }else{
-        mode.src="../images/moon.png ";
-    }
+    mode.src = !wasDarkmode ? "images/sun.png" : "images/moon.png";
 }
 
-load.onload=function(){
-    document.body.classList.toggle('dark-mode', localStorage.getItem('mode') === 'true');
+window.onload=function(){
+    const isDark = localStorage.getItem('mode') === 'true';
+    document.body.classList.toggle("dark-mode", isDark);
+    document.getElementById("mode").src = isDark ? "images/sun.png" : "images/moon.png";
 
-}
+};
 
 
 document.addEventListener("DOMContentLoaded", function () {
